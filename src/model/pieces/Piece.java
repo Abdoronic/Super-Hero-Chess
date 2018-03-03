6e3c6e53fbab4f74bd0d735a754db31daf010b7e
Mark <markmichealhp@outlook.com>
2018-03-03 15:05:10 +0200

    6 and 7

---
+++ src/model/pieces/Piece.java
@@ -0,0 +1,43 @@
+package model.pieces;
+
+public class Piece implements Movable {
+	private String name;
+    private Player owner;
+    private Game game;
+    private int posI;
+    private int posJ;
+    
+    public Piece(Player player, Game game, String name){
+    	this.owner = player;
+    	this.game = game;
+    	this.name = name;
+    }
+
+	public int getPosI() {
+		return posI;
+	}
+
+	public void setPosI(int posI) {
+		this.posI = posI;
+	}
+
+	public int getPosJ() {
+		return posJ;
+	}
+
+	public void setPosJ(int posJ) {
+		this.posJ = posJ;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Player getOwner() {
+		return owner;
+	}
+
+	public Game getGame() {
+		return game;
+	}
+}
