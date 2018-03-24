d74648131cbb90e333facdd0d53b7c6976422b30
Unknown <youssefdiaa.ingame18@gmail.com>
2018-03-24 11:58:18 +0300

    1,2

--- src/model/pieces/Piece.java
+++ src/model/pieces/Piece.java
@@ -2,6 +2,8 @@ package model.pieces;
 
 import model.game.Game;
 import model.game.Player;
+import model.pieces.heroes.Armored;
+import model.pieces.sidekicks.SideKick;
 
 public abstract class Piece implements Movable {
 
@@ -10,11 +12,30 @@ public abstract class Piece implements Movable {
 	private Game game;
 	private int posI;
 	private int posJ;
-
+    
+	
 	public Piece(Player p, Game g, String name) {
 		this.owner = p;
 		this.game = g;
 		this.name = name;
+	}
+	
+	public void attack(Piece target) {
+		Player attacked = target.getOwner();
+		Player attacker = game.getCurrentPlayer();
+		if(target instanceof SideKick) {
+	       	attacker.setSideKilled(attacker.getSideKilled() + 1);
+	       	if(attacker.getSideKilled() % 2 == 0)
+	       		attacker.setPayloadPos(attacker.getPayloadPos() + 1);
+	       	game.getCellAt(target.getPosI(),target.getPosJ()).setPiece(null);
+		} else if(target instanceof Armored && ((Armored) target).isArmorUp()) {
+			((Armored) target).setArmorUp(false);
+		} else {
+			attacked.getDeadCharacters().add(target);
+	       	attacker.setPayloadPos(attacker.getPayloadPos() + 1);
+	       	game.getCellAt(target.getPosI(),target.getPosJ()).setPiece(null);
+		}
+		game.checkWinner();
 	}
 
 	public String getName() {
--- src/model/pieces/sidekicks/SideKick.java
+++ src/model/pieces/sidekicks/SideKick.java
@@ -3,11 +3,31 @@ package model.pieces.sidekicks;
 import model.game.Game;
 import model.game.Player;
 import model.pieces.Piece;
+import model.pieces.heroes.Armored;
 
 public abstract class SideKick extends Piece {
 
 	public SideKick(Player player, Game game, String name) {
 		super(player, game, name);
+	}
+	
+	public void attack(Piece target) {
+		Player attacked = target.getOwner();
+		Player attacker = getGame().getCurrentPlayer();
+		if(target instanceof SideKick) {
+	       	attacker.setSideKilled(attacker.getSideKilled() + 1);
+	       	if(attacker.getSideKilled() % 2 == 0)
+	       		attacker.setPayloadPos(attacker.getPayloadPos() + 1);
+	       	getGame().getCellAt(target.getPosI(),target.getPosJ()).setPiece(null);
+		} else if(target instanceof Armored && ((Armored) target).isArmorUp()) {
+			((Armored) target).setArmorUp(false);
+		} else {
+			attacked.getDeadCharacters().add(target);
+	       	attacker.setPayloadPos(attacker.getPayloadPos() + 1);
+	       	getGame().getCellAt(target.getPosI(),target.getPosJ()).setPiece(null);
+	       	getGame().getCellAt(this.getPosI(),this.getPosJ()).setPiece(null);
+		}
+		getGame().checkWinner();
 	}
 
 	@Override
