91e375a4524e3975331c85240953f3518d542c51
Ronic <abdoronic@gmail.com>
2018-04-05 16:20:26 +0200

    Fixed issue

--- src/model/pieces/sidekicks/SideKick.java
+++ src/model/pieces/sidekicks/SideKick.java
@@ -16,25 +16,26 @@ public abstract class SideKick extends Piece {
 	public SideKick(Player player, Game game, String name) {
 		super(player, game, name);
 	}
-	
+
 	public void attack(Piece target) {
 		super.attack(target);
-		if(getGame().getCellAt(target.getPosI(), target.getPosJ()) != null)
+		if (getGame().getCellAt(target.getPosI(), target.getPosJ()).getPiece() != null)
 			return;
-		Player attacker = getOwner();
-		Cell attackerCell = getGame().getCellAt(this.getPosI(),this.getPosJ());
-		if(target instanceof Armored)
-			attackerCell.setPiece(new Armored(attacker, getGame(), this.getName()));
-		else if(target instanceof Medic)
-			attackerCell.setPiece(new Medic(attacker, getGame(), this.getName()));
-		else if(target instanceof Ranged)
-			attackerCell.setPiece(new Ranged(attacker, getGame(), this.getName()));
-		else if(target instanceof Super)
-			attackerCell.setPiece(new Super(attacker, getGame(), this.getName()));
-		else if(target instanceof Speedster)
-			attackerCell.setPiece(new Speedster(attacker, getGame(), this.getName()));
-		else if(target instanceof Tech)
-			attackerCell.setPiece(new Tech(attacker, getGame(), this.getName()));
+		Cell attackerCell = getGame().getCellAt(getPosI(), getPosJ());
+		if (target instanceof Armored)
+			attackerCell.setPiece(new Armored(getOwner(), getGame(), getName()));
+		else if (target instanceof Medic)
+			attackerCell.setPiece(new Medic(getOwner(), getGame(), getName()));
+		else if (target instanceof Ranged)
+			attackerCell.setPiece(new Ranged(getOwner(), getGame(), getName()));
+		else if (target instanceof Super)
+			attackerCell.setPiece(new Super(getOwner(), getGame(), getName()));
+		else if (target instanceof Speedster)
+			attackerCell.setPiece(new Speedster(getOwner(), getGame(), getName()));
+		else if (target instanceof Tech)
+			attackerCell.setPiece(new Tech(getOwner(), getGame(), getName()));
+		attackerCell.getPiece().setPosI(getPosI());
+		attackerCell.getPiece().setPosJ(getPosJ());
 	}
 
 	@Override
