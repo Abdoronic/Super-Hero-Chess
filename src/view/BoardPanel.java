86cdd2eb5898dcdfeca28500bb4a36ef6995fa4b
Mark <markmichealhp@outlook.com>
2018-04-23 22:58:51 +0200

    boardcell

--- src/view/BoardPanel.java
+++ src/view/BoardPanel.java
@@ -3,19 +3,32 @@ package view;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import model.game.Game;
 import model.pieces.Piece;
+import model.pieces.heroes.Medic;
+import model.pieces.heroes.Ranged;
+import model.pieces.heroes.Super;
+import model.pieces.heroes.Tech;
 import view.customGUI.BoardCell;
 
 @SuppressWarnings("serial")
-public class BoardPanel extends JPanel implements ActionListener{
-	
+public class BoardPanel extends JPanel implements ActionListener {
+
 	private Game game;
+	private static Piece SelectedPiece;
+	private static Boolean SelectedRanged;
+	private static Boolean SelectedSuper;
+	private static Boolean HackedPiece;
+	private static Boolean Teleporting;
+	private static Boolean RestorePiece;
 	private BoardCell[][] board = new BoardCell[6][7];
-	
+
 	public BoardPanel(Game game) {
 		this.game = game;
 		setLayout(new GridLayout(6, 7));
@@ -30,11 +43,11 @@ public class BoardPanel extends JPanel implements ActionListener{
 		}
 		setVisible(true);
 	}
-	
+
 	public Piece getPieceAt(int i, int j) {
 		return game.getCellAt(j, i).getPiece();
 	}
-	
+
 	public boolean isEmpty(int i, int j) {
 		return getPieceAt(i, j) == null;
 	}
@@ -42,6 +55,52 @@ public class BoardPanel extends JPanel implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		System.out.println("here");
+
+	}
+	
+	public void SelectPiece(BoardCell c) { 
+		SelectedPiece.setPosI(c.getI());
+		SelectedPiece.setPosJ(c.getJ());
 		
+	}
+
+	public static void SelectAbility(Piece p) {
+		if (p instanceof Medic) {
+			ArrayList<Piece> dead = p.getOwner().getDeadCharacters();
+			Object[] graveyard = new Object[dead.size()];
+			for (int i = 0; i < dead.size(); i++) {
+				graveyard[i] = dead.get(i).getName();
+			}
+			JPanel revive = new JPanel();
+			revive.add(new JLabel("Please select a dead character to revive"));
+			int pos = JOptionPane.showOptionDialog(null, revive, "Medic", JOptionPane.OK_CANCEL_OPTION,
+					JOptionPane.PLAIN_MESSAGE, null, graveyard, null);
+
+			Piece revived = dead.get(pos);
+			SelectedPiece = revived;
+
+		}
+		if (p instanceof Ranged) {
+			SelectedRanged = true;
+		}
+		if (p instanceof Tech) {
+			Object[] TechAbilities = { "Hack Enemy", "Restore ability", "Teleport" };
+			JPanel abilities = new JPanel();
+			abilities.add(new JLabel("Please a Tech ability"));
+			int pos = JOptionPane.showOptionDialog(null, abilities, "Tech", JOptionPane.OK_CANCEL_OPTION,
+					JOptionPane.PLAIN_MESSAGE, null, TechAbilities, null);
+			if (pos == 0) {
+				HackedPiece = true;
+			}
+			if (pos == 1) {
+				RestorePiece = true;
+			}
+			if (pos == 2) {
+				Teleporting =true;
+			}
+		}
+		if (p instanceof Super) {
+			SelectedSuper = true;
+		}
 	}
 }
